35cc0d6d5168504ed449b93bb7bfdaab92a5bcdd
Matsvei <matvejk97@gmail.com>
2023-06-23 12:44:47 +0200

    chage rhyme alghoritm

--- ConsoleApp1/CountingRhyme.cs
+++ ConsoleApp1/CountingRhyme.cs
@@ -7,33 +7,13 @@
             if (N <= 0 || M <= 0)
                 throw new ArgumentException("N and M must be positive numbers.");
 
-            var items = FillIemsList(N).ToList();
-
-            return PerformCountingToLast(M, items);
-        }
-
-        private IEnumerable<int> FillIemsList(int N)
-        {
+            var currentIndex = 0;
             for (int i = 1; i <= N; i++)
             {
-                yield return i;
-            }
-        }
-
-        private int PerformCountingToLast(int M, List<int> items)
-        {
-            var currentIndex = 0;
-
-            while (items.Count > 1)
-            {
-                var removeIndex = (currentIndex + M - 1) % items.Count;
-
-                items.RemoveAt(removeIndex);
-
-                currentIndex = removeIndex % items.Count;
+                currentIndex = (currentIndex + M) % i;
             }
 
-            return items.SingleOrDefault();
+            return currentIndex + 1;
         }
     }
 }
